b42c63c53b6999d3e11267fc54bbae32378fd800
Andreas Nazlidis <andreas.nazlidis@avision-it.de>
2021-12-28 17:15:51 +0100

    test main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <fstream>
 #include <iostream>
 
+#include "src/tools/tools.h"
+
 using namespace std;
 
-int main() { return 0; }
+int main() {
+  Vec3f color(1., 2., 4.);
+  cout << color;
+
+  return 0;
+}
