25cd763159b1074c44fb5f847224debe313e3559
Andreas Nazlidis <andreas.nazlidis@avision-it.de>
2021-12-28 16:09:39 +0100

    Update

--- src/tools/tools.h
+++ src/tools/tools.h
@@ -1,11 +1,15 @@
 #ifndef TOOLS_H
 #define TOOLS_H
 
+#include <cmath>
+
 #include "mat3.h"
 #include "mat4.h"
 #include "point3.h"
 #include "vec2.h"
 #include "vec3.h"
 #include "vec4.h"
+
+const float PI = acos(-1.);
 
 #endif  // TOOLS_H
