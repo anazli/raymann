dea17cab12dd8806fabb993468b918666e9d444c
Andreas Nazlidis <andreas.nazlidis@avision-it.de>
2022-01-03 22:55:54 +0100

    Updates World test

--- test/testWorld.cpp
+++ test/testWorld.cpp
@@ -18,7 +18,7 @@ TEST_F(Tworld, createsWolrdWithShere) {
   Ray r = Ray(Point3f(0.0f, 0.0f, -5.0f), Vec3f(0.0f, 0.0f, 1.0f));
 
   ASSERT_TRUE(w.intersect(r));
-  // ASSERT_EQ(w.rec.count, 2);
-  // ASSERT_EQ(w.rec.t1, 4.0f);
-  // ASSERT_EQ(w.rec.t2, 6.0f);
+  ASSERT_EQ(s->rec.count, 2);
+  ASSERT_EQ(s->rec.t1, 4.0f);
+  ASSERT_EQ(s->rec.t2, 6.0f);
 }
