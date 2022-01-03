86fa8cc80f731fe45f01a62f17e0966b28c8aaff
Andreas Nazlidis <andreas.nazlidis@avision-it.de>
2022-01-03 22:34:26 +0100

    Updates World test

--- test/testWorld.cpp
+++ test/testWorld.cpp
@@ -5,8 +5,20 @@
 
 using namespace testing;
 
-class Tworld : Test {
+class Tworld : public Test {
  public:
   World w;
-  Sphere s;
 };
+
+TEST_F(Tworld, createsWolrdWithShere) {
+  w = World();
+  Sphere *s = new Sphere(Point3f(0.0f, 0.0f, 0.0f), 1.0f);
+  w.add(s);
+  ASSERT_TRUE(s->getParent() == &w);
+  Ray r = Ray(Point3f(0.0f, 0.0f, -5.0f), Vec3f(0.0f, 0.0f, 1.0f));
+
+  ASSERT_TRUE(w.intersect(r));
+  // ASSERT_EQ(w.rec.count, 2);
+  // ASSERT_EQ(w.rec.t1, 4.0f);
+  // ASSERT_EQ(w.rec.t2, 6.0f);
+}
