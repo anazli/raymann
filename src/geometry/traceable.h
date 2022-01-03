987e8c3b3adfd1a8b5af3acb3e5316fbfe41b16a
Andreas Nazlidis <andreas.nazlidis@avision-it.de>
2022-01-03 20:08:59 +0100

    Update Traceable, world

--- src/geometry/traceable.h
+++ src/geometry/traceable.h
@@ -1,7 +1,7 @@
 #pragma once
 
+#include <list>
 #include <string>
-#include <vector>
 
 #include "tools/ray.h"
 
@@ -15,15 +15,19 @@ class Traceable {
  public:
   virtual ~Traceable() {}
   virtual bool intersect(const Ray &r) = 0;
-  virtual void add(Traceable *) {}
-  virtual void remove(Traceable *) {}
+  virtual void add(Traceable *item) {}
+  virtual void remove(Traceable *item) {}
+  virtual bool isWorld() const { return false; }
   std::string name() const { return m_name; }
+  void setParent(Traceable *t) { m_parent = t; }
+  Traceable *getParent() const { return m_parent; }
 
   record rec;
 
  protected:
   Traceable() {}
-  Traceable(const std::string &);
+  Traceable(const std::string &n);
+  Traceable *m_parent;
 
  private:
   std::string m_name;
@@ -32,13 +36,26 @@ class Traceable {
 class World : public Traceable {
  public:
   virtual ~World();
-  bool intersect(const Ray &r) override;
-  void add(Traceable *) override;
-  void remove(Traceable *) override;
+  bool intersect(const Ray &r) override {
+    std::list<Traceable *>::iterator it;
+    for (it = m_traceable_list.begin(); it != m_traceable_list.end(); ++it)
+      return (*it)->intersect(r);
+    return false;
+  }
+  void add(Traceable *item) override {
+    m_traceable_list.push_back(item);
+    item->setParent(this);
+  }
+  void remove(Traceable *item) override {
+    m_traceable_list.remove(item);
+    item->setParent(nullptr);
+  }
+  bool isWorld() const override { return true; }
 
  protected:
-  World(const std::string &);
+  World() {}
+  World(const std::string &n);
 
  private:
-  std::vector<Traceable *> m_traceable_list;  // TODO: or maybe list
+  std::list<Traceable *> m_traceable_list;  // TODO: or maybe list
 };
