f8df91900e8f4c63d4f62e5c94eb6bed8a8cda8e
Andreas Nazlidis <andreas.nazlidis@avision-it.de>
2022-01-05 23:21:44 +0100

    Sphere transformation tests

--- test/testSphere.cpp
+++ test/testSphere.cpp
@@ -56,3 +56,25 @@ TEST_F(Tsphere, SphereIsBehindOrigin) {
   ASSERT_EQ(s.rec.t1, -6.0f);
   ASSERT_EQ(s.rec.t2, -4.0f);
 }
+
+TEST_F(Tsphere, testingTheNextTest) {
+  s = Sphere(Point3f(0.0f, 0.0f, 0.0f), 1.0f);
+  r = Ray(Point3f(0.0f, 0.0f, -5.0f), Vec3f(0.0f, 0.0f, 1.0f));
+  Ray r1 = r.transform(scale(2.0f, 2.0f, 2.0f).inverse());
+  ASSERT_TRUE(s.intersect(r1));
+  ASSERT_EQ(s.rec.count, 2);
+  ASSERT_EQ(s.rec.t1, 3.0f);
+  ASSERT_EQ(s.rec.t2, 7.0f);
+}
+
+TEST_F(Tsphere, transformsSphere) {
+  r = Ray(Point3f(0.0f, 0.0f, -5.0f), Vec3f(0.0f, 0.0f, 1.0f));
+  Traceable *t = new TraceableTransformer(
+      new Sphere(Point3f(0.0f, 0.0f, 0.0f), 1.0f), scale(2.0f, 2.0f, 2.0f));
+  ASSERT_TRUE(t->intersect(r));
+  std::cout << t->name() << std::endl;
+  // ASSERT_EQ(t->rec.count, 2);
+  // ASSERT_EQ(trans->rec.t1, 3.0f);
+  // ASSERT_EQ(trans->rec.t2, 7.0f);
+  delete t;
+}
