d9a975786fbcfa5ff32ea89d44df10161e3949d6
Andreas Nazlidis <andreas.nazlidis@avision-it.de>
2022-01-06 17:00:57 +0100

    More cleenup

--- src/tools/vec3.h
+++ src/tools/vec3.h
@@ -8,52 +8,34 @@
 
 #include "vec4.h"
 
-// std::mt19937 gen(13759327u);
-
-/*! \brief A simple 3D vector class.
- *
- *  The Vec3 class creates a 3D vector (x,y,z) and provides some useful
- * operations such as dot, cross product, normalization, random vectors. It also
- * supports all the operations such as multiplication, addition, subtraction,
- * division etc.
- *
- */
-
 template <class T>
 class Vec3 {
  public:
-  //! Constructor with default arguments. If no parameters
-  //! are provided, a vector (0,0,0) is created. Otherwise
-  //! a vector (x,y,z) is created.
   Vec3<T>(const T& p1 = 0., const T& p2 = 0., const T& p3 = 0.)
       : m_x{p1}, m_y{p2}, m_z{p3} {}
 
-  //! Copy Constructor. Creates a vector that is a copy
-  //! of an existing vector provided as argument.
   Vec3<T>(const Vec3<T>& v) : m_x{v.x()}, m_y{v.y()}, m_z{v.z()} {}
   Vec3<T>(const Vec4<T>& v) : m_x{v.x()}, m_y{v.y()}, m_z{v.z()} {}
 
-  // Getters
-  T x() const { return m_x; }  //!< returns the x component.
-  T y() const { return m_y; }  //!< returns the y component.
-  T z() const { return m_z; }  //!< returns the z component.
+  T x() const { return m_x; }
+  T y() const { return m_y; }
+  T z() const { return m_z; }
 
-  // Setters
-  void setX(const T& p) { m_x = p; }  //!< sets the x component.
-  void setY(const T& p) { m_y = p; }  //!< sets the y component.
-  void setZ(const T& p) { m_z = p; }  //!< sets the z component.
+  void setX(const T& p) { m_x = p; }
+  void setY(const T& p) { m_y = p; }
+  void setZ(const T& p) { m_z = p; }
 
   void setXYZ(const T& num) {
     setX(num);
     setY(num);
     setZ(num);
-  }  //!< sets the three components to a specific value.
+  }
 
   void setXYZ(const T& p1, const T& p2, const T& p3) {
     setX(p1);
     setY(p2);
     setZ(p3);
-  }  //!< Every component gets a unique value.
+  }
 
   T operator[](int i) const {
     assert(i >= 0 && i <= 2);
@@ -83,41 +65,22 @@ class Vec3 {
     return *this;
   }
 
-  Vec3<T> operator+() const {
-    return Vec3<T>(m_x, m_y, m_z);
-  };  //!< Overloaded unary (+) operator. Returns the same vector.
-  Vec3<T> operator-() const {
-    return Vec3<T>(-m_x, -m_y, -m_z);
-  }  //!< Overloaded unary (-) operator. Returns the opposite vector.
+  Vec3<T> operator+() const { return Vec3<T>(m_x, m_y, m_z); };
+  Vec3<T> operator-() const { return Vec3<T>(-m_x, -m_y, -m_z); }
 
-  // Prefix
-  Vec3<T>& operator++();  //!< Overloaded prefix (++) operator. Returns the
-                          //!< vector and increments its components by one.
-  Vec3<T>& operator--();  //!< Overloaded prefix (--) operator. Returns the
-                          //!< vector and decrements its components by one.
-  // Postfix
-  Vec3<T> operator++(int);  //!< Overloaded postfix (++) operator. Increments
-                            //!< the vectors components by one and returns it.
-  Vec3<T> operator--(int);  //!< Overloaded postfix (--) operator. Decrements
-                            //!< the vectors components by one and returns it.
+  Vec3<T>& operator++();
+  Vec3<T>& operator--();
+  Vec3<T> operator++(int);
+  Vec3<T> operator--(int);
 
-  Vec3<T>& operator+=(
-      const T& num);  //!< Overloaded unary (+=) operator. Increments the
-                      //!< vector's components by a value num.
+  Vec3<T>& operator+=(const T& num);
   Vec3<T>& operator+=(const Vec3<T>& v);
-  Vec3<T>& operator-=(
-      const T& num);  //!< Overloaded unary (-=) operator. Decrements the
-                      //!< vector's components by a value num.
+  Vec3<T>& operator-=(const T& num);
   Vec3<T>& operator-=(const Vec3<T>& v);
-  Vec3<T>& operator*=(
-      const T& num);  //!< Overloaded unary (*=) operator. Multiplies the
-                      //!< vector's components by a value num.
+  Vec3<T>& operator*=(const T& num);
 
-  Vec3<T>& normalize();  //!< Makes the vector a unit vector of length one.
-  double length() const  //!< Returns the length of the vector.
-  {
-    return sqrt(x() * x() + y() * y() + z() * z());
-  }
+  Vec3<T>& normalize();
+  double length() const { return sqrt(x() * x() + y() * y() + z() * z()); }
 
   bool isValid() {
     if (m_x * 0. != m_x * 0.) {
@@ -239,129 +202,80 @@ inline std::ostream& operator<<(std::ostream& out, const Vec3<T>& v) {
 //--------------------------------------------
 
 template <typename T>
-Vec3<T> operator+(const Vec3<T>& v1,
-                  const Vec3<T>& v2)  //!< Overloaded binary (+) operator.
-                                      //!< Addition of two vectors.
-{
+Vec3<T> operator+(const Vec3<T>& v1, const Vec3<T>& v2) {
   return Vec3<T>(v1.x() + v2.x(), v1.y() + v2.y(), v1.z() + v2.z());
 }
 
 template <typename T>
-Vec3<T> operator+(const Vec3<T>& v,
-                  const T& num)  //!< Overloaded binary (+) operator. Addition
-                                 //!< of !< a vector and a number.
-{
+Vec3<T> operator+(const Vec3<T>& v, const T& num) {
   return Vec3<T>(v.x() + num, v.y() + num, v.z() + num);
 }
 
 template <typename T>
-Vec3<T> operator+(const T& num,
-                  const Vec3<T>& v)  //!< Overloaded binary (+) operator.
-                                     //!< Addition of a !< vector and a number.
-{
+Vec3<T> operator+(const T& num, const Vec3<T>& v) {
   return v + num;
 }
 
 template <typename T>
-Vec3<T> operator-(const Vec3<T>& v1,
-                  const Vec3<T>& v2)  //!< Overloaded binary (-) operator. !<
-                                      //!< Subtraction of two vectors.
-{
+Vec3<T> operator-(const Vec3<T>& v1, const Vec3<T>& v2) {
   return Vec3<T>(v1.x() - v2.x(), v1.y() - v2.y(), v1.z() - v2.z());
 }
 
 template <typename T>
-Vec3<T> operator-(const Vec3<T>& v,
-                  const T& num)  //!< Overloaded binary (-) operator.
-                                 //!< Subtraction !< of a vector and a number.
-{
+Vec3<T> operator-(const Vec3<T>& v, const T& num) {
   return Vec3<T>(v.x() - num, v.y() - num, v.z() - num);
 }
 
 template <typename T>
-Vec3<T> operator-(
-    const T& num,
-    const Vec3<T>& v)  //!< Overloaded binary (-) operator. Subtraction !< of a
-                       //!< vector and a number.
-{
+Vec3<T> operator-(const T& num, const Vec3<T>& v) {
   return v - num;
 }
 
 template <typename T>
-Vec3<T> operator*(const Vec3<T>& v1,
-                  const Vec3<T>& v2)  //!< Overloaded binary (*) operator. !<
-                                      //!< Multiplication of two vectors.
-{
+Vec3<T> operator*(const Vec3<T>& v1, const Vec3<T>& v2) {
   return Vec3<T>(v1.x() * v2.x(), v1.y() * v2.y(), v1.z() * v2.z());
 }
 
 template <typename T>
-Vec3<T> operator*(const Vec3<T>& v,
-                  const T& num)  //!< Overloaded binary (*) operator.
-                                 //!< Multiplication of a vector and a number.
-{
+Vec3<T> operator*(const Vec3<T>& v, const T& num) {
   return Vec3<T>(v.x() * num, v.y() * num, v.z() * num);
 }
 
 template <typename T>
-Vec3<T> operator*(
-    const T& num,
-    const Vec3<T>& v)  //!< Overloaded binary (*) operator.
-                       //!< Multiplication of a vector and a number.
-{
+Vec3<T> operator*(const T& num, const Vec3<T>& v) {
   return v * num;
 }
 
 template <typename T>
-Vec3<T> operator/(
-    const Vec3<T>& v1,
-    const Vec3<T>& v2)  //!< Overloaded binary (/) operator. Division of
-                        //!< two vectors component by component.
-{
+Vec3<T> operator/(const Vec3<T>& v1, const Vec3<T>& v2) {
   if (v2.x() == 0. || v2.y() == 0. || v2.z() == 0.) throw "Division by zero!";
   return Vec3<T>(v1.x() / v2.x(), v1.y() / v2.y(), v1.z() / v2.z());
 }
 
 template <typename T>
-Vec3<T> operator/(const Vec3<T>& v,
-                  const T& num)  //!< Overloaded binary (/) operator. Division
-                                 //!< of a vector's components by a number.
-{
-  double e = 1.E-30;
+Vec3<T> operator/(const Vec3<T>& v, const T& num) {
+  T e = 1.E-30;
   return Vec3<T>(v.x() / (num + e), v.y() / (num + e), v.z() / (num + e));
 }
 
 template <typename T>
-bool operator==(const Vec3<T>& v1,
-                const Vec3<T>& v2)  //!< Overloaded binary (==) operator. Checks
-                                    //!< if two vectors are identical.
-{
+bool operator==(const Vec3<T>& v1, const Vec3<T>& v2) {
   return (v1.x() == v2.x() && v1.y() == v2.y() && v1.z() == v2.z());
 }
 
 template <typename T>
-bool operator!=(const Vec3<T>& v1,
-                const Vec3<T>& v2)  //!< Overloaded binary (!=) operator. Checks
-                                    //!< if two vectors are not identical.
-{
+bool operator!=(const Vec3<T>& v1, const Vec3<T>& v2) {
   return !(v1 == v2);
 }
 
 template <typename T>
-T dot(const Vec3<T>& v1,
-      const Vec3<T>& v2)  //!< The dot product of two vectors.
-                          //!< Returns a number of type double.
-{
+T dot(const Vec3<T>& v1, const Vec3<T>& v2) {
   Vec3<T> v = v1 * v2;
   return v.x() + v.y() + v.z();
 }
 
 template <typename T>
-Vec3<T> cross(
-    const Vec3<T>& v1,
-    const Vec3<T>& v2)  //!< The cross product of two vectors. Returns a
-                        //!< vector perpendicular to the surface of them.
-{
+Vec3<T> cross(const Vec3<T>& v1, const Vec3<T>& v2) {
   T x = v1.y() * v2.z() - v1.z() * v2.y();
   T y = v1.z() * v2.x() - v1.x() * v2.z();
   T z = v1.x() * v2.y() - v1.y() * v2.x();
@@ -369,10 +283,7 @@ Vec3<T> cross(
 }
 
 template <typename T>
-Vec3<T> getUnitVectorOf(
-    const Vec3<T>& v)  //!< Gets the unit vector of a vector
-                       //!< without changing the provided vector.
-{
+Vec3<T> getUnitVectorOf(const Vec3<T>& v) {
   return v / (v.length() + 1.E-30);
 }
 
