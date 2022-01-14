188ece316be76eb9ccb8e0306fe487bc698e0289
Andreas Nazlidis <andreas.nazlidis@avision-it.de>
2022-01-14 15:19:00 +0100

    Adds Red lighting test

--- test/testMat.cpp
+++ test/testMat.cpp
@@ -39,3 +39,44 @@ TEST_F(TMat, createsNewLight2) {
   ASSERT_EQ(light.intensity().y(), 4.0f);
   ASSERT_EQ(light.intensity().z(), 0.0f);
 }
+
+TEST_F(TMat, lightsWithEyeBetweenLightAndSurface) {
+  //----------------------------------
+  // As it is in Material->lighting
+  //----------------------------------
+
+  Vec3f eye(0.0f, 0.0f, -1.0f);
+  light = PointLight(Point3f(0.0f, 0.0f, -10.0f), Vec3f(1.0f));
+
+  Point3f p(0.0f);
+  Vec3f m_color(1.0f);
+  Vec3f effective_color = m_color * light.intensity();
+  Vec3f lightv = (light.position() - p).normalize();
+  float m_ambient = 0.1f;
+  float m_diffuse = 0.9f;
+  float m_specular = 0.9f;
+  float m_shininess = 200.0f;
+  Vec3f norm(0.0f, 0.0f, -1.0f);  // norm == this->normal(p)
+
+  Vec3f ret_ambient = effective_color * m_ambient;
+  Vec3f ret_diffuse;
+  Vec3f ret_specular;
+
+  float light_normal = dot(lightv, norm);
+  if (light_normal > 0.0f) {
+    ret_diffuse = effective_color * m_diffuse * light_normal;
+    Vec3f reflectv = reflect(-lightv, norm);
+    float reflect_dot_eye = dot(reflectv, eye);
+    if (reflect_dot_eye > 0.0f) {
+      float factor = pow(reflect_dot_eye, m_shininess);
+      ret_specular = light.intensity() * m_specular * factor;
+    }
+  }
+
+  Vec3f result = ret_ambient + ret_diffuse + ret_specular;
+
+  ASSERT_EQ(result.x(), 1.9f);
+  ASSERT_EQ(result.y(), 1.9f);
+  ASSERT_EQ(result.z(), 1.9f);
+  ASSERT_TRUE(result == Vec3f(1.9f));
+}
