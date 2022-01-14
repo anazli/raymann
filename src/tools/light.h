23c318fb0058bf42b0892655e3f37c52c4dba667
Andreas Nazlidis <andreas.nazlidis@avision-it.de>
2022-01-14 12:05:34 +0100

    Adds PointLight

---
+++ src/tools/light.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "point3.h"
+#include "vec3.h"
+
+class PointLight {
+ public:
+  PointLight() {}
+  PointLight(const Point3f &pos, const Vec3f &inten)
+      : m_position(pos), m_intensity(inten) {}
+
+  void setPosition(const Point3f &pos) { m_position = pos; }
+  void setIntensity(const Vec3f &inten) { m_intensity = inten; }
+  Point3f position() const { return m_position; }
+  Vec3f intensity() const { return m_intensity; }
+
+ private:
+  Point3f m_position;
+  Vec3f m_intensity;
+};
