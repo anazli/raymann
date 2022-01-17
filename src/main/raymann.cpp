598afd9199f41351deb013aa9b6cf6b48069027f
Andreas Nazlidis <andreas.nazlidis@avision-it.de>
2022-01-17 23:59:48 +0100

    Update main

--- src/main/raymann.cpp
+++ src/main/raymann.cpp
@@ -9,11 +9,13 @@ using namespace std;
 
 int main() {
   PointLight light(Point3f(-15.0f, 15.0f, -15.0f), Vec3f(0.7f, 0.7f, 0.7f));
-  Traceable *m =
-      new Material(new Transformer(new Sphere(), scale(0.8f, 1.0f, 1.0f)),
-                   Vec3f(0.2f, 1.0f, 0.2f));
+
+  Traceable *sphere = new Sphere();
+  sphere = new Material(new Transformer(sphere, scale(0.8f, 1.0f, 1.0f)),
+                        Vec3f(0.2f, 1.0f, 0.2f));
+
   World w;
-  w.add(m);
+  w.add(sphere);
 
   Ray r(Point3f(0.0f, 0.0f, -5.0f), Vec3f(0.0f, 0.0f, 10.0f));
 
