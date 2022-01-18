8b9ac3db75c594b9b946cdf99836556c2eb6838c
Andreas Nazlidis <andreas.nazlidis@avision-it.de>
2022-01-18 21:38:50 +0100

    Traceable implementation file

--- src/geometry/traceable.h
+++ src/geometry/traceable.h
@@ -5,8 +5,6 @@
 
 #include "tools/tools.h"
 
-// TODO: Refactoring in an implementation file
-
 class Traceable {
  public:
   virtual ~Traceable() {}
@@ -70,17 +68,7 @@ class Transformer : public TraceableDeco {
   }
   std::string name() const override { return TraceableDeco::name(); }
   Record record() const override { return TraceableDeco::record(); }
-  Vec3f normal(const Point3f &p) const override {
-    Vec4f v4(p);
-    v4 = m_transformer.inverse() * v4;
-    Point3f object_point = v4;
-    Vec3f object_normal = object_point - Point3f(0.0f, 0.0f, 0.0f);
-    v4 = object_normal;
-    v4 = m_transformer.inverse().transpose() * v4;
-    Vec3f world_normal = v4;
-    return world_normal.normalize();
-    // return TraceableDeco::normal(p);
-  }
+  Vec3f normal(const Point3f &p) const override;
 
  private:
   Mat4f m_transformer;
@@ -90,37 +78,11 @@ class Material : public TraceableDeco {
  public:
   Material(Traceable *tr, const Vec3f &c = Vec3f(1.0f, 1.0f, 1.0f),
            float am = 0.1f, float diff = 0.9f, float spec = 0.9f,
-           float shi = 10.0f)
-      : TraceableDeco(tr),
-        m_color(c),
-        m_ambient(am),
-        m_diffuse(diff),
-        m_specular(spec),
-        m_shininess(shi) {}
+           float shi = 10.0f);
 
   bool intersect(const Ray &r) override { return TraceableDeco::intersect(r); }
   Vec3f lighting(const PointLight &light, const Point3f &p,
-                 const Vec3f &eye) override {
-    Vec3f effective_color = m_color * light.intensity();
-    Vec3f lightv = (light.position() - p).normalize();
-
-    Vec3f ret_ambient = effective_color * this->m_ambient;
-    Vec3f ret_diffuse;
-    Vec3f ret_specular;
-
-    float light_normal = dot(lightv, this->normal(p));
-    if (light_normal > 0.0f) {
-      ret_diffuse = effective_color * m_diffuse * light_normal;
-      Vec3f reflectv = reflect(-lightv, normal(p));
-      float reflect_dot_eye = dot(reflectv, eye);
-      if (reflect_dot_eye > 0.0f) {
-        float factor = 1.E-10f * pow(reflect_dot_eye, m_shininess);
-        ret_specular = light.intensity() * m_specular * factor;
-      }
-    }
-
-    return ret_ambient + ret_diffuse + ret_specular;
-  }
+                 const Vec3f &eye) override;
   std::string name() const override { return TraceableDeco::name(); }
   Record record() const override { return TraceableDeco::record(); }
   Vec3f normal(const Point3f &p) const override {
@@ -139,40 +101,12 @@ class World : public Traceable {
  public:
   World() {}
   World(const std::string &n);
-  virtual ~World() {
-    std::list<Traceable *>::iterator it;
-    for (it = m_traceable_list.begin(); it != m_traceable_list.end(); ++it)
-      delete (*it);
-  }
-  bool intersect(const Ray &r) override {
-    bool has_intersection = false;
-    std::list<Traceable *>::iterator it;
-    for (it = m_traceable_list.begin(); it != m_traceable_list.end(); ++it)
-      if ((*it)->intersect(r)) has_intersection = true;
-    return has_intersection;
-  }
-  void add(Traceable *item) override {
-    m_traceable_list.push_back(item);
-    item->setParent(this);
-  }
-  void remove(Traceable *item, bool del = true) override {
-    m_traceable_list.remove(item);
-    item->setParent(nullptr);
-    if (del) delete item;
-  }
+  virtual ~World();
+  bool intersect(const Ray &r) override;
+  void add(Traceable *item) override;
+  void remove(Traceable *item, bool del = true) override;
   bool isWorld() const override { return true; }
-  Traceable &closestHit() override {
-    std::list<Traceable *>::const_iterator it;
-    Traceable *temp = nullptr;
-    float min_hit = MAXFLOAT;
-    for (it = m_traceable_list.begin(); it != m_traceable_list.end(); ++it)
-      if ((*it)->record().t_min() > 0.0f && (*it)->record().t_min() < min_hit) {
-        temp = (*it);
-        min_hit = temp->record().t_min();
-      }
-    assert(temp != nullptr);  // All intersections are negative
-    return *temp;
-  }
+  Traceable &closestHit() override;
   std::list<Traceable *>::iterator createIterator() {
     return m_traceable_list.begin();
   }
