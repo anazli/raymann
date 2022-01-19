2600431a599fae837a72981aece7ae0841c18fa8
Andreas Nazlidis <andreas.nazlidis@avision-it.de>
2022-01-20 00:09:42 +0100

    Adds random samples

--- src/container/canvas.cpp
+++ src/container/canvas.cpp
@@ -8,27 +8,31 @@ using std::list;
 using std::ofstream;
 
 void Canvas::render(Traceable *world, const Ray &r, const PointLight &light) {
+  int num_samples = 50;
   for (int j = 0; j < height(); ++j) {
     for (int i = 0; i < width(); ++i) {
       float wall_z = r.direction().z();
       float wall_size = 7.0f;
       float pixel_size = wall_size / (float)width();
       float half = wall_size / 2.0f;
-      float world_y = half - pixel_size * j;
-      float world_x = -half + pixel_size * i;
 
-      Point3f position(world_x, world_y, wall_z);
-      Ray ray(r.origin(), (position - r.origin()).normalize());
       Vec3f color;
+      for (int s = 0; s < num_samples; ++s) {
+        float world_y = half - pixel_size * (j + drand48());
+        float world_x = -half + pixel_size * (i + drand48());
+        Point3f position(world_x, world_y, wall_z);
+        Ray ray(r.origin(), (position - r.origin()).normalize());
 
-      if (world->intersect(ray)) {
-        Traceable &closest = world->closestHit();
-        Point3f point = ray.position(closest.record().t_min());
-        Vec3f eye = -r.direction();
-        color = closest.lighting(light, point, eye);
-      } else {
-        color = Vec3f();
+        if (world->intersect(ray)) {
+          Traceable &closest = world->closestHit();
+          Point3f point = ray.position(closest.record().t_min());
+          Vec3f eye = -r.direction();
+          color += closest.lighting(light, point, eye);
+        } else {
+          color += Vec3f(0.1, 0.1, 0.2);
+        }
       }
+      color = color / (float)num_samples;
       writePixel(i, j, color);
     }
   }
