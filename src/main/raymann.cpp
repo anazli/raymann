f443edc0d96e602f11d5e0d6b90804d1fee039be
Andreas Nazlidis <andreas.nazlidis@avision-it.de>
2022-01-20 00:10:07 +0100

    Updates main scene

--- src/main/raymann.cpp
+++ src/main/raymann.cpp
@@ -14,6 +14,13 @@ int main() {
   sphere = new Material(new Transformer(sphere, scale(0.8f, 1.0f, 1.0f)),
                         Vec3f(0.2f, 1.0f, 0.2f));
 
+  /*sphere = new Transformer(sphere, translation(-0.6f, 0.0f, 0.0f));
+  Traceable *sphere2 = new Sphere();
+  sphere2 = new Material(new Transformer(sphere2, scale(0.5f, 1.0f, 1.0f)),
+                         Vec3f(0.2f, 1.0f, 0.2f));
+  sphere2 = new Transformer(sphere2, translation(0.6f, 0.0f, 0.0f));
+  */
+
   Traceable *w = new World();
   w->add(sphere);
 
