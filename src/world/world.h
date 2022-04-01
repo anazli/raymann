a530c836add55e25cf5cd86509594d442af9e446
Andreas Naz <andreas221b@gmail.com>
2022-04-01 13:18:05 +0200

    Remove methods

--- src/world/world.h
+++ src/world/world.h
@@ -14,17 +14,6 @@ class World : public Traceable {
   bool isWorld() const override { return true; }
   Traceable &closestHit() override;
   std::vector<float> intersectionsSorted() const;
-  Traceable *createSphere(std::shared_ptr<SphereBuilder> b,
-                          const Point3f &center = Point3f(0.0f, 0.0f, 0.0f),
-                          const float &radius = 1.0f,
-                          const Vec3f &color = Vec3f(1.0f, 1.0f, 1.0f),
-                          float am = 0.1f, float diff = 0.9f, float spec = 0.9f,
-                          float shi = 10.0f);
-  Traceable *createTransformedSphere(
-      std::shared_ptr<SphereBuilder> b, const Mat4f &matrix,
-      const Point3f &center = Point3f(0.0f, 0.0f, 0.0f),
-      const float &radius = 1.0f, const Vec3f &color = Vec3f(1.0f, 1.0f, 1.0f),
-      float am = 0.1f, float diff = 0.9f, float spec = 0.9f, float shi = 10.0f);
 
  private:
   std::list<std::shared_ptr<Traceable>> m_traceable_list;
