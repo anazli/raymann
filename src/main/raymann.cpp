c173aa6f0dbbc10534ff8610067653ace4d84406
Andreas Naz <andreas221b@gmail.com>
2022-04-01 13:35:48 +0200

    Updates main

--- src/main/raymann.cpp
+++ src/main/raymann.cpp
@@ -4,35 +4,29 @@
 #include "container/canvas.h"
 #include "geometry/sphere.h"
 #include "tools/tools.h"
+#include "world/scene.h"
 #include "world/world.h"
 
 using namespace std;
 
 int main() {
+  Scene scene;
   PointLight light(Point3f(-15.0f, 15.0f, -15.0f), Vec3f(0.7f, 0.7f, 0.7f));
 
-  Traceable *sphere = new Sphere();
-  sphere = new Material(new Transformer(sphere, scale(0.8f, 1.0f, 1.0f)),
-                        Vec3f(0.09f, 0.172f, 0.909f));
+  shared_ptr<SphereBuilder> builder;
+  builder.reset(new StandardSphere);
 
-  /*sphere = new Transformer(sphere, translation(-0.6f, 0.0f, 0.0f));
-  Traceable *sphere2 = new Sphere();
-  sphere2 = new Material(new Transformer(sphere2, scale(0.5f, 1.0f, 1.0f)),
-                         Vec3f(0.2f, 1.0f, 0.2f));
-  sphere2 = new Transformer(sphere2, translation(0.6f, 0.0f, 0.0f));
-  */
-
-  unique_ptr<Traceable> w;
-  w.reset(new World());
-
-  shared_ptr<Traceable> shared_sphere;
-  shared_sphere.reset(sphere);
-  w->add(shared_sphere);
+  shared_ptr<Traceable> sphere = scene.createTransformedSphere(
+      builder, scale(0.8f, 1.0f, 1.0f), Vec3f(0.09f, 0.172f, 0.909f));
 
   Ray r(Point3f(0.0f, 0.0f, -5.0f), Vec3f(0.0f, 0.0f, 10.0f));
 
   Canvas canvas(600, 600);
   canvas.setFileName("scenes/scene.ppm");
+
+  unique_ptr<Traceable> w;
+  w.reset(new World());
+  w->add(sphere);
 
   canvas.render(move(w), r, light);
   canvas.save();
