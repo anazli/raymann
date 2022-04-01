03502d1ba6c43e138c29357a24141d24d1e97a47
Andreas Naz <andreas221b@gmail.com>
2022-04-01 14:07:06 +0200

    Updates tests

--- test/testBuilder.cpp
+++ test/testBuilder.cpp
@@ -1,17 +1,16 @@
 #include "geometry/sphere.h"
 #include "gtest/gtest.h"
+#include "world/scene.h"
 #include "world/world.h"
 
 using namespace testing;
 
 TEST(SphereBuilder, CreateSimpleSphere) {
-  std::unique_ptr<World> world;
+  Scene scene;
   std::shared_ptr<StandardSphere> builder;
 
-  Traceable *test = world->createSphere(builder);
-  std::shared_ptr<Traceable> sphere =
-      builder->getSphere();  // not really neccessary, in case we want to use
-                             // sphere in another world
+  std::shared_ptr<Traceable> test = scene.createSphere(builder);
+  std::shared_ptr<Traceable> sphere = builder->getSphere();
 
-  ASSERT_TRUE(test == sphere.get());
+  ASSERT_TRUE(test == sphere);
 }
