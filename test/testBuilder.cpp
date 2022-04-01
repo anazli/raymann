e72292386f8f64003a91e88e1ce46d93f10b1174
Andreas Naz <andreas221b@gmail.com>
2022-04-01 09:32:55 +0200

    Adds red builder test

---
+++ test/testBuilder.cpp
@@ -0,0 +1,13 @@
+#include "geometry/sphere.h"
+#include "gtest/gtest.h"
+#include "world/world.h"
+
+using namespace testing;
+
+TEST(SphereBuilder, CreateSimpleSphere) {
+  std::unique_ptr<World> world;
+  StandardSphere builder;
+
+  world->createSphere(builder);
+  std::shared_ptr<Sphere> sphere = builder.getSphere();
+}
