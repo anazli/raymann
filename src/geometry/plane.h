853cb7589b7c0914a21af4d57b24295e57007e27
Andreas Nazlidis <andreas221b@gmail.com>
2022-05-23 16:45:08 +0200

    Cleenup

--- src/geometry/plane.h
+++ src/geometry/plane.h
@@ -4,9 +4,8 @@
 
 class Plane : public Traceable {
  public:
-  Plane(const float x = 0, const float z = 0) : m_x(x), m_z(z) {}
+  Plane() = default;
   virtual ~Plane() {}
-
   bool isWorld() const override { return false; }
   bool intersect(const Ray &r) override {
     if (fabs(r.direction().y()) < 0.0001f) {
@@ -17,13 +16,10 @@ class Plane : public Traceable {
     rec.count = 1;
     return true;
   }
-
   Vec3f normal(const Point3f &p) const override {
     return Vec3f(0.0f, 1.0f, 0.0f);
   }
-
   Record record() const override { return rec; }
-
   bool isShadowed(const PointLight &l, const Point3f &p) override {
     Vec3f v = l.position() - p;
     float distance = v.length();
@@ -32,7 +28,4 @@ class Plane : public Traceable {
       return true;
     return false;
   }
-
- private:
-  float m_x, m_z;
 };
--- test/testPlane.cpp
+++ test/testPlane.cpp
@@ -11,7 +11,7 @@ class Tplane : public Test {
 };
 
 TEST_F(Tplane, normalOfPlaneIsConstantEverywhere) {
-  p = Plane(100, 100);
+  p = Plane();
   Vec3f n1 = p.normal(Point3f(0.0f, 0.0f, 0.0f));
   Vec3f n2 = p.normal(Point3f(10.0f, 0.0f, -10.0f));
   Vec3f n3 = p.normal(Point3f(-5.0f, 0.0f, 150.0f));
@@ -22,21 +22,21 @@ TEST_F(Tplane, normalOfPlaneIsConstantEverywhere) {
 }
 
 TEST_F(Tplane, intersectRayParallelToThePlane) {
-  p = Plane(100, 100);
+  p = Plane();
   r = Ray(Point3f(0.0f, 10.0f, 0.0f), Vec3f(0.0f, 0.0f, 1.0f));
   ASSERT_FALSE(p.intersect(r));
   ASSERT_TRUE(p.record().count == 0);
 }
 
 TEST_F(Tplane, intersectWithACoplanarRay) {
-  p = Plane(100, 100);
+  p = Plane();
   r = Ray(Point3f(0.0f, 0.0f, 0.0f), Vec3f(0.0f, 0.0f, 1.0f));
   ASSERT_FALSE(p.intersect(r));
   ASSERT_TRUE(p.record().count == 0);
 }
 
 TEST_F(Tplane, rayIntersectingPlaneFromAbove) {
-  p = Plane(100, 100);
+  p = Plane();
   r = Ray(Point3f(0.0f, 1.0f, 0.0f), Vec3f(0.0f, -1.0f, 0.0f));
   ASSERT_TRUE(p.intersect(r));
   ASSERT_TRUE(p.record().count == 1);
@@ -44,7 +44,7 @@ TEST_F(Tplane, rayIntersectingPlaneFromAbove) {
 }
 
 TEST_F(Tplane, rayIntersectingPlaneFromBelow) {
-  p = Plane(100, 100);
+  p = Plane();
   r = Ray(Point3f(0.0f, -1.0f, 0.0f), Vec3f(0.0f, 1.0f, 0.0f));
   ASSERT_TRUE(p.intersect(r));
   ASSERT_TRUE(p.record().count == 1);
