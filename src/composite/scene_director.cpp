bce044b6c89b489b3cce62e23d1ce61e1306ddf1
Andreas Nazlidis <andreas221b@gmail.com>
2022-10-18 20:00:41 +0200

    Consider center/radius when adding sphere with director

--- src/composite/scene_director.cpp
+++ src/composite/scene_director.cpp
@@ -11,43 +11,47 @@ StandardSphere::StandardSphere(std::shared_ptr<TraceableBuilder> builder,
     : SceneDirector(builder, prop) {}
 
 std::shared_ptr<Traceable> StandardSphere::create() {
-  m_builder->addTraceable(new Sphere);
+  m_builder->addTraceable(
+      new Sphere(m_properties.sphereCenter(), m_properties.sphereRadius()));
   m_builder->setProperties(m_properties);
   m_builder->addTransformer();
   m_builder->addMaterial();
   return m_builder->getTraceable();
 }
 
-CheckerSphere::CheckerSphere(
-    std::shared_ptr<TraceableBuilder> builder, const Properties& prop)
+CheckerSphere::CheckerSphere(std::shared_ptr<TraceableBuilder> builder,
+                             const Properties& prop)
     : SceneDirector(builder, prop) {}
 
 std::shared_ptr<Traceable> CheckerSphere::create() {
-  m_builder->addTraceable(new Sphere);
+  m_builder->addTraceable(
+      new Sphere(m_properties.sphereCenter(), m_properties.sphereRadius()));
   m_builder->setProperties(m_properties);
   m_builder->addTransformer();
   m_builder->addCheckerPattern();
   return m_builder->getTraceable();
 }
 
-RingSphere::RingSphere(
-    std::shared_ptr<TraceableBuilder> builder, const Properties& prop)
+RingSphere::RingSphere(std::shared_ptr<TraceableBuilder> builder,
+                       const Properties& prop)
     : SceneDirector(builder, prop) {}
 
 std::shared_ptr<Traceable> RingSphere::create() {
-  m_builder->addTraceable(new Sphere);
+  m_builder->addTraceable(
+      new Sphere(m_properties.sphereCenter(), m_properties.sphereRadius()));
   m_builder->setProperties(m_properties);
   m_builder->addTransformer();
   m_builder->addRingPattern();
   return m_builder->getTraceable();
 }
 
-GradientSphere::GradientSphere(
-    std::shared_ptr<TraceableBuilder> builder, const Properties& prop)
+GradientSphere::GradientSphere(std::shared_ptr<TraceableBuilder> builder,
+                               const Properties& prop)
     : SceneDirector(builder, prop) {}
 
 std::shared_ptr<Traceable> GradientSphere::create() {
-  m_builder->addTraceable(new Sphere);
+  m_builder->addTraceable(
+      new Sphere(m_properties.sphereCenter(), m_properties.sphereRadius()));
   m_builder->setProperties(m_properties);
   m_builder->addTransformer();
   m_builder->addGradientPattern();
@@ -66,8 +70,8 @@ std::shared_ptr<Traceable> StandardPlane::create() {
   return m_builder->getTraceable();
 }
 
-CheckerPlane::CheckerPlane(
-    std::shared_ptr<TraceableBuilder> builder, const Properties& prop)
+CheckerPlane::CheckerPlane(std::shared_ptr<TraceableBuilder> builder,
+                           const Properties& prop)
     : SceneDirector(builder, prop) {}
 
 std::shared_ptr<Traceable> CheckerPlane::create() {
@@ -78,8 +82,8 @@ std::shared_ptr<Traceable> CheckerPlane::create() {
   return m_builder->getTraceable();
 }
 
-RingPlane::RingPlane(
-    std::shared_ptr<TraceableBuilder> builder, const Properties& prop)
+RingPlane::RingPlane(std::shared_ptr<TraceableBuilder> builder,
+                     const Properties& prop)
     : SceneDirector(builder, prop) {}
 
 std::shared_ptr<Traceable> RingPlane::create() {
