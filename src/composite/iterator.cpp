37781f5b6c0d6f0c655966a5ebe7e781af28ec92
Andreas <andreas221b@gmail.com>
2024-02-05 21:53:35 +0100

    move iterator

---
+++ src/composite/iterator.cpp
@@ -0,0 +1,30 @@
+#include "composite/iterator.h"
+
+WorldIterator::WorldIterator(const SceneElementContainer &world)
+    : m_sceneElementContainer(world) {}
+
+bool WorldIterator::first() {
+  m_current = m_sceneElementContainer.begin();
+  if (*m_current)
+    return true;
+  else
+    return false;
+}
+
+SceneElementContainer::const_iterator WorldIterator::begin() const {
+  return m_sceneElementContainer.begin();
+}
+
+SceneElementContainer::const_iterator WorldIterator::end() const {
+  return m_sceneElementContainer.end();
+}
+
+void WorldIterator::advance() { m_current++; }
+
+bool WorldIterator::notDone() {
+  return m_current != m_sceneElementContainer.end();
+}
+
+SceneElementRawPtr WorldIterator::currentElement() {
+  return (*m_current).get();
+}
