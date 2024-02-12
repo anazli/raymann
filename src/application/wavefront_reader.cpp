2b39cb4cac3ac92f716ebb8d38f462db2c60cc07
Andreas <andreas221b@gmail.com>
2024-02-12 18:57:42 +0100

    move implementation

---
+++ src/application/wavefront_reader.cpp
@@ -0,0 +1,114 @@
+#include "application/wavefront_reader.h"
+
+#include <algorithm>
+#include <sstream>
+
+#include "application/error.h"
+
+using std::isspace;
+using std::istringstream;
+using std::string;
+using std::string_view;
+using std::vector;
+
+bool isVertexEntry(const string_view &line) {
+  return !line.empty() && line[0] == 'v' && isspace(line[1]);
+}
+
+bool isTriangleEntry(const string_view &line) {
+  return !line.empty() && line[0] == 'f' && line.length() == 7;
+}
+
+bool isPolygonEntry(const string_view &line) {
+  return !line.empty() && line[0] == 'f' && line.length() == 11;
+}
+
+WavefrontReader::WavefrontReader(const string_view &file) { openFile(file); }
+
+void WavefrontReader::parseInput() {
+  if (m_inputStream.is_open()) {
+    string line;
+    while (getline(m_inputStream, line)) {
+      if (isVertexEntry(line)) {
+        parseVertexEntry(line);
+      } else if (isTriangleEntry(line)) {
+        parseTriangleEntry(line);
+      } else if (isPolygonEntry(line)) {
+        parsePolygonEntry(line);
+      }
+    }
+  }
+  m_inputStream.close();
+}
+
+void WavefrontReader::openFile(const string_view &file) {
+  if (!file.empty()) {
+    m_inputStream.open(file.data());
+  }
+}
+
+vector<Vec3f> WavefrontReader::vertexCollection() const { return m_vertices; }
+
+vector<Triangle> WavefrontReader::triangleCollection() const {
+  return m_triangles;
+}
+
+void WavefrontReader::parseVertexEntry(const string_view &line) {
+  istringstream ss(line.data());
+  string v, x, y, z;
+  if (ss >> v >> x >> y >> z) {
+    try {
+      m_vertices.push_back(Vec3f(stof(x), stof(y), stof(z)));
+    } catch (...) {
+      APP_MSG(line.data());
+      APP_ASSERT(false,
+                 "Error while parsing the line above in wavefront file!");
+    }
+  }
+}
+
+void WavefrontReader::parseTriangleEntry(const string_view &line) {
+  istringstream ss(line.data());
+  string v, i, j, k;
+  if (ss >> v >> i >> j >> k) {
+    try {
+      Point3f p1 = Point3f(m_vertices[stol(i) - 1]);
+      Point3f p2 = Point3f(m_vertices[stol(j) - 1]);
+      Point3f p3 = Point3f(m_vertices[stol(k) - 1]);
+      m_triangles.push_back(Triangle({p1, p2, p3}));
+    } catch (...) {
+      APP_MSG(line.data());
+      APP_ASSERT(false,
+                 "Error while parsing the line above in wavefront file!");
+    }
+  }
+}
+
+void WavefrontReader::parsePolygonEntry(const string_view &line) {
+  istringstream ss(line.data());
+  string v, i, j, k, f, g;
+  if (ss >> v >> i >> j >> k >> f >> g) {
+    try {
+      vector<Vec3f> polygonVertices;
+      polygonVertices.push_back(m_vertices[stol(i) - 1]);
+      polygonVertices.push_back(m_vertices[stol(j) - 1]);
+      polygonVertices.push_back(m_vertices[stol(k) - 1]);
+      polygonVertices.push_back(m_vertices[stol(f) - 1]);
+      polygonVertices.push_back(m_vertices[stol(g) - 1]);
+      triangulatePolygon(polygonVertices);
+    } catch (...) {
+      APP_MSG(line.data());
+      APP_ASSERT(false,
+                 "Error while parsing the line above in wavefront file!");
+    }
+  }
+}
+
+void WavefrontReader::triangulatePolygon(vector<Vec3f> vertices) {
+  for (int i = 1; i < vertices.size() - 1; ++i) {
+    Point3f p1 = Point3f(vertices[0]);
+    Point3f p2 = Point3f(vertices[i]);
+    Point3f p3 = Point3f(vertices[i + 1]);
+    m_triangles.push_back(Triangle({p1, p2, p3}));
+  }
+}
--- src/application/wavefront_reader.h
+++ src/application/wavefront_reader.h
@@ -1,115 +1,27 @@
 #pragma once
 
-#include <algorithm>
 #include <fstream>
-#include <sstream>
 #include <string_view>
 
-#include "application/error.h"
 #include "geometry/triangle.h"
 #include "tools/vec3.h"
 
-bool isVertexEntry(const std::string_view& line) {
-  return !line.empty() && line[0] == 'v' && std::isspace(line[1]);
-}
-
-bool isTriangleEntry(const std::string_view& line) {
-  return !line.empty() && line[0] == 'f' && line.length() == 7;
-}
-
-bool isPolygonEntry(const std::string_view& line) {
-  return !line.empty() && line[0] == 'f' && line.length() == 11;
-}
-
 class WavefrontReader {
  public:
-  WavefrontReader(const std::string_view& file = "") { openFile(file); }
+  WavefrontReader(const std::string_view& file = "");
 
-  void parseInput() {
-    if (m_inputStream.is_open()) {
-      std::string line;
-      while (std::getline(m_inputStream, line)) {
-        if (isVertexEntry(line)) {
-          parseVertexEntry(line);
-        } else if (isTriangleEntry(line)) {
-          parseTriangleEntry(line);
-        } else if (isPolygonEntry(line)) {
-          parsePolygonEntry(line);
-        }
-      }
-    }
-    m_inputStream.close();
-  }
+  void parseInput();
 
-  void openFile(const std::string_view& file) {
-    if (!file.empty()) {
-      m_inputStream.open(file.data());
-    }
-  }
+  void openFile(const std::string_view& file);
 
-  std::vector<Vec3f> vertexCollection() const { return m_vertices; }
-  std::vector<Triangle> triangleCollection() const { return m_triangles; }
+  std::vector<Vec3f> vertexCollection() const;
+  std::vector<Triangle> triangleCollection() const;
 
  private:
-  void parseVertexEntry(const std::string_view& line) {
-    std::istringstream ss(line.data());
-    std::string v, x, y, z;
-    if (ss >> v >> x >> y >> z) {
-      try {
-        m_vertices.push_back(Vec3f(stof(x), stof(y), stof(z)));
-      } catch (...) {
-        APP_MSG(line.data());
-        APP_ASSERT(false,
-                   "Error while parsing the line above in wavefront file!");
-      }
-    }
-  }
-
-  void parseTriangleEntry(const std::string_view& line) {
-    std::istringstream ss(line.data());
-    std::string v, i, j, k;
-    if (ss >> v >> i >> j >> k) {
-      try {
-        Point3f p1 = Point3f(m_vertices[stol(i) - 1]);
-        Point3f p2 = Point3f(m_vertices[stol(j) - 1]);
-        Point3f p3 = Point3f(m_vertices[stol(k) - 1]);
-        m_triangles.push_back(Triangle({p1, p2, p3}));
-      } catch (...) {
-        APP_MSG(line.data());
-        APP_ASSERT(false,
-                   "Error while parsing the line above in wavefront file!");
-      }
-    }
-  }
-
-  void parsePolygonEntry(const std::string_view& line) {
-    std::istringstream ss(line.data());
-    std::string v, i, j, k, f, g;
-    if (ss >> v >> i >> j >> k >> f >> g) {
-      try {
-        std::vector<Vec3f> polygonVertices;
-        polygonVertices.push_back(m_vertices[stol(i) - 1]);
-        polygonVertices.push_back(m_vertices[stol(j) - 1]);
-        polygonVertices.push_back(m_vertices[stol(k) - 1]);
-        polygonVertices.push_back(m_vertices[stol(f) - 1]);
-        polygonVertices.push_back(m_vertices[stol(g) - 1]);
-        triangulatePolygon(polygonVertices);
-      } catch (...) {
-        APP_MSG(line.data());
-        APP_ASSERT(false,
-                   "Error while parsing the line above in wavefront file!");
-      }
-    }
-  }
-
-  void triangulatePolygon(std::vector<Vec3f> vertices) {
-    for (int i = 1; i < vertices.size() - 1; ++i) {
-      Point3f p1 = Point3f(vertices[0]);
-      Point3f p2 = Point3f(vertices[i]);
-      Point3f p3 = Point3f(vertices[i + 1]);
-      m_triangles.push_back(Triangle({p1, p2, p3}));
-    }
-  }
+  void parseVertexEntry(const std::string_view& line);
+  void parseTriangleEntry(const std::string_view& line);
+  void parsePolygonEntry(const std::string_view& line);
+  void triangulatePolygon(std::vector<Vec3f> vertices);
 
   std::ifstream m_inputStream;
   std::vector<Vec3f> m_vertices;
