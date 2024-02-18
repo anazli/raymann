534b5133e1964e38e2fd07c6dfa2241a80258c06
Andreas <andreas221b@gmail.com>
2024-02-18 19:56:48 +0100

    update scene

--- src/main/raymann.cpp
+++ src/main/raymann.cpp
@@ -18,55 +18,41 @@ using namespace std;
 int main() {
   auto light = PointLight(Point3f(80.f, 70.f, 200.f), Vec3f(1.f, 1.f, 1.f));
 
-  //----------------------Teapot----------------
-
   WavefrontReader reader("dragon.obj");
   reader.addLightForModel(light);
-  auto prop = MaterialProperties{};
-  prop.setProperty(Props::SPECULAR, 0.4f).setProperty(Props::SHININESS, 5.f);
-  reader.addMaterial(make_unique<ConstantTexture>(Vec3f(0.164f, 0.292f, 0.8f)),
-                     prop);
+  auto dragon_props = MaterialProperties{};
+  dragon_props.setProperty(Props::SPECULAR, 0.4f)
+      .setProperty(Props::SHININESS, 5.f);
+  reader.addMaterial(make_unique<ConstantTexture>(Vec3f(0.08f, 0.683f, 0.382f)),
+                     dragon_props);
   reader.parseInput();
   SceneElementPtr world = reader.getStructureBVHierarchy();
-  cout << "Done reading file..." << endl;
-  cout << "Number of children:" << world->getChildren().size() << endl;
 
   BuilderPtr builder = make_unique<WorldBuilder>();
-  auto prop_floor = MaterialProperties{};
-  prop_floor.setProperty(Props::SPECULAR, 0.4f)
+  auto floor_props = MaterialProperties{};
+  floor_props.setProperty(Props::SPECULAR, 0.4f)
       .setProperty(Props::REFLECTION, 0.f);
   builder->processSceneElement(new Plane);
   builder->applyTransformation(translation(0.f, -40.f, 0.f) *
-                               rotationOverY(0.31415f));
+                               rotationOverY(PI));
   builder->applyMaterial(
-      make_unique<ConstantTexture>(Vec3f(0.08f, 0.55f, 0.08f)), prop_floor);
+      make_unique<ConstantTexture>(Vec3f(0.35f, 0.35f, 0.65f)), floor_props);
   SceneElementPtr floor(builder->getCurrentElement());
   world->add(floor);
-
-  // builder->processSceneElement(new Plane);
-  // builder->applyTransformation(translation(0.f, 0.f, -140.f) *
-  // rotationOverX(-1.7708f) * rotationOverY(0.2f));
-  // prop_floor.setProperty(Props::REFLECTION,
-  // 0.5f); builder->applyMaterial(
-  //     make_unique<ConstantTexture>(Vec3f(0.55f,
-  //     0.55f, 0.55f)), prop_floor);
-  // SceneElementPtr
-  // wall(builder->getCurrentElement());
-  // world->add(wall);
 
   //----------------------------------------------------------------------------
   auto canvas = Canvas(1200, 800);
   canvas.setFileName("scenes/scene.ppm");
   BaseCameraPtr camera =
-      make_unique<Camera>(canvas.width(), canvas.height(), 0.9f);
+      make_unique<Camera>(canvas.width(), canvas.height(), 1.5f);
   camera->computePixelSize();
-  auto from = Point3f(90.f, 50.f, 210.f);
-  auto to = Point3f(0.f, 0.0f, -30.f);
+  auto from = Point3f(0.f, -15.f, 130.f);
+  auto to = Point3f(30.f, 30.0f, -40.f);
   auto up = Vec3f(0.0f, 1.0f, 0.0f);
   camera->setTransform(view_transform(from, to, up));
 
   BaseRendererPtr renderer = make_unique<PhongModel>();
-  renderer->setBackgroundColor(Vec3f(0.4f, 0.4f, 0.4f));
+  renderer->setBackgroundColor(Vec3f(0.5f, 0.3f, 0.3f));
   chrono::time_point<chrono::steady_clock> start = chrono::steady_clock::now();
   canvas.render(world, std::move(camera), std::move(renderer));
   canvas.save();
