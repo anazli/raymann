89e7cfae7c73c3736be581846ea4fc2aa8778ac9
Andreas Nazlidis <andreas221b@gmail.com>
2025-02-15 13:57:24 +0100

    add bxdf and lambertian distributions

--- src/distribution/bxdf.cpp
+++ src/distribution/bxdf.cpp
@@ -1,13 +1,35 @@
 #include "bxdf.h"
 
+#include "distribution/utilities.h"
+
 Spectrum Bxdf::rhd(const Vec3D& wo, int num_samples, Vec2D& samples) const {
-  return Spectrum();
+  Spectrum r(0.f);
+  /*for (int i = 0; i < num_samples; ++i) {
+    // Estimate one term of $\rho_\roman{hd}$
+    Vec3D wi;
+    auto pdf = 0.f;
+    Spectrum f = Sample_f(w, &wi, u[i], &pdf);
+    if (pdf > 0) r += f * absoluteCosTheta(wi) / pdf;
+  }*/
+  return r / num_samples;
 }
 
 Spectrum Bxdf::rhh(int num_samples, Vec2D& samples1, Vec2D& samples2) const {
-  return Spectrum();
+  Spectrum r(0.f);
+  /*for (int i = 0; i < num_samples; ++i) {
+    // Estimate one term of $\rho_\roman{hh}$
+    Vec3D wo, wi;
+    wo = UniformSampleHemisphere(u1[i]);
+    float pdfo = UniformHemispherePdf(), pdfi = 0;
+    Spectrum f = Sample_f(wo, &wi, u2[i], &pdfi);
+    if (pdfi > 0)
+      r += f * absoluteCosTheta(wi) * absoluteCosTheta(wo) / (pdfo * pdfi);
+  }*/
+  return r / (PI * num_samples);
 }
 
-float Bxdf::pdf(const Vec3D& wi, const Vec3D& wo) const { return 0.0f; }
+float Bxdf::pdf(const Vec3D& wi, const Vec3D& wo) const {
+  return isOnSameHemisphere(wo, wi) ? absoluteCosTheta(wi) * InvPI : 0;
+}
 
 Bxdf::Bxdf(const BxdfType& type) : m_bsdf_type(type) {}
--- src/distribution/lambertian_brdf.cpp
+++ src/distribution/lambertian_brdf.cpp
@@ -1,5 +1,6 @@
 #include "distribution/lambertian_brdf.h"
 
+#include "lambertian_brdf.h"
 #include "tools/tools.h"
 
 LambertianBrdf::LambertianBrdf(const Spectrum& sp)
@@ -7,4 +8,14 @@ LambertianBrdf::LambertianBrdf(const Spectrum& sp)
 
 Spectrum LambertianBrdf::f(const Vec3D& wo, const Vec3D& wi) const {
   return m_spectrum * InvPI;
+}
+
+Spectrum LambertianBrdf::rhd(const Vec3D& wo, int num_samples,
+                             Vec2D& samples) const {
+  return m_spectrum;
+}
+
+Spectrum LambertianBrdf::rhh(int num_samples, Vec2D& samples1,
+                             Vec2D& samples2) const {
+  return m_spectrum;
 }
