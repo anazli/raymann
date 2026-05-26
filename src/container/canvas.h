7d5b0a4e04fa2cbb17f5d5c7c51699665cf444c0
Andreas Nazlidis <andreas221b@gmail.com>
2026-05-26 22:16:19 +0200

    add multi-threading support

--- src/container/canvas.h
+++ src/container/canvas.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <thread>
 #include <vector>
 
 #include "camera/camera.h"
@@ -10,11 +11,18 @@ class BaseCamera;
 
 class Canvas {
  public:
-  Canvas(Camera &&camera, std::unique_ptr<BaseRenderer> renderer);
+  Canvas(Camera &&camera, std::unique_ptr<BaseRenderer> renderer,
+         int num_threads = 4);
+  Canvas(Canvas &&) = default;
+  Canvas &operator=(Canvas &&) = default;
+  Canvas(const Canvas &) = delete;
+  Canvas &operator=(const Canvas &) = delete;
+
   int width() const;
   int height() const;
   std::string fileName() const;
   void setFileName(const std::string &fn);
+  void setNumThreads(int num_threads);
   void render(SceneElementPtr &&world);
   void renderParallel(SceneElementPtr &&world);
   void writePixel(int x, int y, const Vec3f &color);
@@ -22,8 +30,11 @@ class Canvas {
   void save();
 
  private:
+  void renderRegion(int y_start, int y_end, const SceneElementPtr &world);
+
   Camera m_camera;
   std::unique_ptr<BaseRenderer> m_renderer;
   std::string m_fileName;
   std::vector<std::vector<Vec3f>> m_pixels;
+  int m_num_threads;
 };
